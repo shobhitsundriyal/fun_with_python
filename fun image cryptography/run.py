745b13ce4a768429b45256416fe3420dc6ac4913
unknown <shobhitsundriyal0170@gmail.com>
2020-03-04 23:53:28 +0530

    imread

--- fun image cryptography/run.py
+++ fun image cryptography/run.py
@@ -10,3 +10,13 @@ def encoder(key):
 def decoder(key):
 
     return
+
+key = int(input('Enter a integer key: '))
+random.seed(key)
+np.random.seed(key)
+
+# for now just read image
+image = cv2.imread('img.jpg')
+
+cv2.imshow('op',image)
+cv2.waitKey()
