6a812a0eee04d9c2d45dc6e0d0ecd823f9c0526d
unknown <shobhitsundriyal0170@gmail.com>
2020-09-05 09:21:28 +0530

    now its all good

--- Mess up clipboard/script.py
+++ Mess up clipboard/script.py
@@ -1,7 +1,20 @@
-import tkinter as tk
+import pyperclip as pc
+from random import choice as ch
 
-root = tk.Tk()
-root.withdraw()
-data = root.clipboard_get()
+def mess_it_up(txt):
+    lis = ['1','3','2']
+    l = len(txt)
+    mod_txt = ''
+    for i in range(l):
+        c = ch(lis)
+        mod_txt = mod_txt + c
+    return mod_txt
 
-print(data)
+text = pc.paste()
+while True:
+    new_text = pc.paste()
+    if new_text != text:
+        new_text = mess_it_up(new_text)
+        text = new_text
+        pc.copy(text)
+        print(text)
