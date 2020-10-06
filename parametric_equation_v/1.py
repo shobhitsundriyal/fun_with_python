332549c9c39f92620a8c679cff63873ef39928c4
unknown <shobhitsundriyal0170@gmail.com>
2020-10-07 00:15:32 +0530

    parametric visualization done

--- parametric_equation_v/1.py
+++ parametric_equation_v/1.py
@@ -19,22 +19,41 @@ t = 0
 
 def translate(x,y):
     return x+(win_width//2), y+(win_height//2)
-def x_pt(t):
-    return int(math.sin(t/10) * 100) 
 
-def y_pt(t):
-    return int(math.cos(t/10) * 100) #sin(freq)*amplitude
+def x_pt1(t):
+    result = math.sin(t/10) * 100 + math.sin(t/15) * 120 + math.sin(t/25) * 5
+    return int(result) 
+
+def y_pt1(t): #sin(freq)*amplitude
+    result = math.cos(t/10) * 100
+    return int(result)
+
+def x_pt2(t):
+    result = math.sin(t/10) * 200 + math.sin(t) * 2
+    return int(result) 
+
+def y_pt2(t): 
+    result = math.cos(t/20) * 200 + math.cos(t/12) * 30 + math.cos(t/8) * 22
+    return int(result)
 
 def redraw():
     global t
-    #background.fill(black)
-    x,y = translate(x_pt(t), y_pt(t))
-    pygame.draw.circle(background, white, (x, y), 1) 
-    t += 1
+    #if mod%20 == 0:
+    background.fill(black)
+    '''
+    pygame.draw.circle(background, white, (x1, y1), 1) 
+    pygame.draw.circle(background, white, (x2, y2), 1) 
+    '''
+    for i in range(20):#number of trails
+        x1,y1 = translate(x_pt1(t+i), y_pt1(t+i))
+        x2,y2 = translate(x_pt2(t+i), y_pt2(t+i))
+        pygame.draw.line(background, white , (x1,y1),(x2, y2), 2)
+    t += 1.5
     
     pygame.display.update()
 
 #Main Loop
+
 while not out:
     clock.tick(120)             
  
