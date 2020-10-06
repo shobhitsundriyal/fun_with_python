7d35805e8023eae110f96b6ef3dec5da70ca9673
unknown <shobhitsundriyal0170@gmail.com>
2020-10-06 23:36:50 +0530

    circle using sin and cos

--- parametric_equation_v/1.py
+++ parametric_equation_v/1.py
@@ -1,7 +1,8 @@
+#refecrence - https://www.youtube.com/watch?v=LaarVR1AOvs
 import pygame
 import math
 
-win_width, win_height = 1280, 720
+win_width, win_height = 720, 720
 out = False
 
 #Colors
@@ -14,8 +15,22 @@ pygame.init()
 background = pygame.display.set_mode((win_width, win_height))
 clock = pygame.time.Clock()
 
+t = 0
+
+def translate(x,y):
+    return x+(win_width//2), y+(win_height//2)
+def x_pt(t):
+    return int(math.sin(t/10) * 100) 
+
+def y_pt(t):
+    return int(math.cos(t/10) * 100) #sin(freq)*amplitude
+
 def redraw():
-    
+    global t
+    #background.fill(black)
+    x,y = translate(x_pt(t), y_pt(t))
+    pygame.draw.circle(background, white, (x, y), 1) 
+    t += 1
     
     pygame.display.update()
 
