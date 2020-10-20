10a9f8a9d60a32c1c60772c5c1b4d3c3cf1bb3b6
unknown <shobhitsundriyal0170@gmail.com>
2020-10-20 10:17:41 +0530

    done

--- dimension_fun/simple_3d_cube.py
+++ dimension_fun/simple_3d_cube.py
@@ -1,15 +1,24 @@
 import pygame
-import math
+from math import sin, cos, radians
 import numpy as np
+from numpy import transpose as T
 
 win_width, win_height = 800, 800
 out = False
 
-cube_points = np.empty((4,3))
-cube_points[0] = [-50, -50, 0]
-cube_points[1] = [50, -50, 0]
-cube_points[2] = [50, 50, 0]
-cube_points[3] = [-50, 50, 0]
+cube_points = np.empty((8,3))
+cube_points[0] = [-50, -50, -50]
+cube_points[1] = [50, -50, -50]
+cube_points[2] = [50, 50, -50]
+cube_points[3] = [-50, 50, -50]
+cube_points[4] = [-50, -50, 50]
+cube_points[5] = [50, -50, 50]
+cube_points[6] = [50, 50, 50]
+cube_points[7] = [-50, 50, 50]
+
+
+angle = radians(1.3)
+angle_plus = radians(5)
 
 #Colors
 white = (255,255,255)
@@ -23,21 +32,57 @@ clock = pygame.time.Clock()
 
 
 ortho_mat = np.array([
-        [1, 0, 0]
+        [1, 0, 0],
         [0, 1, 0]
     ])
+
+rotateZ = np.array([
+    [cos(angle), -sin(angle), 0],
+    [sin(angle), cos(angle), 0 ],
+    [0, 0, 1]
+])
+
+rotateX = np.array([
+    [1, 0, 0],
+    [0, cos(angle), -sin(angle)],
+    [0, sin(angle), cos(angle)],
+])
+
+rotateY = np.array([
+    [cos(angle), 0, -sin(angle)],
+    [0, 1, 0],
+    [sin(angle), 0, cos(angle)],
+])
 
 def translate(x):
     x = x + win_width/2
     return int(x)
 
+def connect(p1, p2, projected_points):
+    global background
+    pygame.draw.line(background, white, projected_points[p1], projected_points[p2], 2)
+
 def redraw(): # Clean up the screen and start a new grid and new frame of pendulum with new coordinates
     background.fill(black)
-    cube_points = np.matmul()
-    for points in cube_points:
-        x = translate(points[0])
-        y = translate(points[1])
+    global cube_points
+
+    cube_points = T(np.matmul(rotateZ, T(cube_points)))
+    cube_points = T(np.matmul(rotateY, T(cube_points)))
+    cube_points = T(np.matmul(rotateZ, T(cube_points)))
+    cube_points = T(np.matmul(rotateX, T(cube_points)))
+
+    # projection at last
+    projected_points = T(np.matmul(ortho_mat, T(cube_points)))
+    projected_points += win_height/2
+    for points in projected_points:
+        x = int(points[0])
+        y = int(points[1])
         pygame.draw.circle(background, white, (x,y), 5)
+
+    for i in range(4):
+        connect(i, (i+1)%4, projected_points)
+        connect(i, i+4, projected_points)
+        connect(i+4, ((i+1)%4)+4, projected_points)
 
     pygame.display.update()
 
